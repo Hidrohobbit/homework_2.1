c256e346f1e29a2cc1683f20475fb00ac5716be4
Stas Mishin <hidrohobbit@yandex.ru>
2022-09-28 18:34:18 +0400

    changing file opening check

--- homework_2.1.cpp
+++ homework_2.1.cpp
@@ -22,39 +22,30 @@ int main()
     std::ifstream fin ("in.txt");
     std::ofstream fout("out.txt");
 
-    fin >> arr_size;
-    file_in_array = create_array(arr_size);
+    
 
-
-    if (fin.is_open())
+    if ( fin.is_open() )
     {
+
+        fin >> arr_size;
+        file_in_array = create_array(arr_size);
+
 
         for (int i = 0; i < arr_size; ++i)
         {
             fin >> file_in_array[i];
         }
-    }
-    else
-        std::cout << "\nНе удалось открыть файл \"in.txt\"!";
 
 
-    fin >> arr_size_second;
-    file_in_array_second = create_array(arr_size_second);
+        fin >> arr_size_second;
+        file_in_array_second = create_array(arr_size_second);
 
-    if (fin.is_open())
-    {
         for (int i = 0; i < arr_size_second; ++i)
         {
             fin >> file_in_array_second[i];
         }
-    }
-    else
-        std::cout << "\nНе удалось открыть файл \"in.txt\"!";
+        
 
-    
-
-    if (fout.is_open())
-    {
         fout << arr_size_second << std::endl;
         fout << file_in_array_second[arr_size_second - 1] << " ";
 
@@ -63,23 +54,25 @@ int main()
             fout << file_in_array_second[i] << " ";
         }
         fout << std::endl;
-    }
-    else
-        std::cout << "\nНе удалось открыть файл \"out.txt\" ";
+        
 
-    if (fout.is_open())
-    {
         fout << arr_size << std::endl;
-        //fout << file_in_array[arr_size - 1] << " ";
+        
 
-        for (int i = 1; i < arr_size ; ++i)
+        for (int i = 1; i < arr_size; ++i)
         {
             fout << file_in_array[i] << " ";
         }
+
         fout << file_in_array[0];
+        
     }
     else
-        std::cout << "\nНе удалось открыть файл \"out.txt\" ";
+    {
+        std::cout << "\nНе удалось открыть \"in.txt\" ";
+    }
+
+    
     
 
 
